278070ffdce2d0e4a313a9b6e5c35eab558577fa
Matthew Matlock <matt.matlock@gmail.com>
2026-03-26 14:42:13 -0500

    Automatically decode some data. Additional functions for working with compound and spectra data

--- pydx/db.py
+++ pydx/db.py
@@ -1,38 +1,20 @@
 import sqlite3
 import pandas as pd
-from .parsers import decode_gap_fill_status, decode_spectrum
+from .parsers import decode_gap_fill_status, decode_gap_status, decode_mol_structure, decode_peak_areas, decode_peak_ratings, decode_retention_times, decode_spectrum, decode_xml_spectrum
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, inspect
 from typing import List, Tuple, Union
 
-sample_type_codes = {
-    0: 'Sample',
-    1: 'Unknown',
-    2: 'Blank',
-    3: 'Quality Control',
-    4: 'Identification Only'
-}
-
-class LazyBlob(object):
-    def __init__(self, data, decoder):
-        self._data = data
-        self._decoder = decoder
-        self._decoded = False
-
-    @property
-    def data(self):
-        if not self._decoded:
-            self._data = self._decoder(self._data)
-            self._decoded = True
-        return self._data
-
 class PyDX(object):
+    
     def __init__(self, path):
         self.path = path
         self.engine = create_engine(f"sqlite:///{self.path}")
         Base = automap_base()
         Base.prepare(autoload_with=self.engine)
     
+    
+    """The following properties return tables with sample data"""
     @property
     def inputs(self):
         if not hasattr(self, '_workflow_data'):
@@ -45,18 +27,62 @@ class PyDX(object):
             self._samples_data = pd.read_sql_table('StudyInformation', con=self.engine)
         return self._samples_data
         
+        
+    """The following properties access feature data and decode peak data vectors and molecular structures"""
+    def _decode_feature_vectors(self, features_df):
+        features_df[['Area', 'AreaFlags']] = features_df[['Area']].apply(lambda row: decode_peak_areas(row['Area']), axis=1, result_type='expand')
+        features_df[['PeakRating', 'PeakRatingFlags']] = features_df[['PeakRating']].apply(lambda row: decode_peak_ratings(row['PeakRating']), axis=1, result_type='expand')
+        features_df['GapFillStatus'] = features_df['GapFillStatus'].apply(decode_gap_fill_status)
+        features_df['GapStatus'] = features_df['GapStatus'].apply(decode_gap_status)
+        features_df['MolStructure'] = features_df['MolStructure'].apply(decode_mol_structure)
+        return features_df
+    
     @property
     def features(self):
         if not hasattr(self, '_features_data'):
             self._features_data = pd.read_sql_table('ConsolidatedUnknownCompoundItems', con=self.engine, index_col='ID')
-        return self._features_data
+            self._decode_feature_vectors(self._features_data)
+        return self._features_data 
         
-    @property
-    def spectra(self):
-        if not hasattr(self, '_spectra_data'):
-            self._spectra_data = pd.read_sql_table('MassSpectrumItems', con=self.engine)
-        return self._spectra_data
+    def get_feature_by_name(self, name):
+        tab = pd.read_sql_query(f"SELECT * FROM ConsolidatedUnknownCompoundItems WHERE Name = '{name}'", con=self.engine)
+        self._decode_feature_vectors(tab)
+        return tab
     
+    def get_features_by_id(self, feature_ids):
+        df = None
+        for batch in self.iterate_features(feature_ids):
+            if df is None:
+                df = batch
+            else:
+                df = pd.concat([df, batch], ignore_index=True)
+        return df
+    
+    def iterate_features(self, feature_ids=None, batch_size=1000):
+        if feature_ids is None:
+            feature_ids = pd.read_sql_query("SELECT ID FROM ConsolidatedUnknownCompoundItems", con=self.engine)['ID'].tolist()
+        for i in range(0, len(feature_ids), batch_size):
+            batch_ids = feature_ids[i:i+batch_size]
+            if len(batch_ids) == 1:
+                where_clause = f"ID = {batch_ids[0]}"
+            else:
+                where_clause = f"ID IN {tuple(batch_ids)}"
+            tab = pd.read_sql_query(f"SELECT * FROM ConsolidatedUnknownCompoundItems WHERE {where_clause}", con=self.engine)
+            self._decode_feature_vectors(tab)
+            yield tab
+            
+    def count_features(self):
+        result = pd.read_sql_query("SELECT COUNT(*) AS count FROM ConsolidatedUnknownCompoundItems", con=self.engine)
+        return result['count'].iloc[0]
+
+    @property
+    def feature_ids(self):
+        if not hasattr(self, '_features_ids'):
+            result = pd.read_sql_query("SELECT ID FROM ConsolidatedUnknownCompoundItems", con=self.engine)
+            self._features_ids = result['ID'].tolist()
+        return self._features_ids
+
+    """The following properties return chromatogram data and retention time correction curves"""
     @property
     def chromatograms(self):
         if not hasattr(self, '_chromatograms_data'):
@@ -67,6 +93,8 @@ class PyDX(object):
     def corrected_retention_times(self):
         if not hasattr(self, '_corrected_retention_times_data'):
             self._corrected_retention_times_data = pd.read_sql_table('FileAlignmentCorrectionItems', con=self.engine)
+            self._corrected_retention_times_data['OriginalRT'] = self._corrected_retention_times_data['OriginalRT'].apply(lambda blb: decode_retention_times(blb))
+            self._corrected_retention_times_data['CorrectedRT'] = self._corrected_retention_times_data['CorrectedRT'].apply(lambda blb: decode_retention_times(blb))
         return self._corrected_retention_times_data
     
     
@@ -81,20 +109,24 @@ class PyDX(object):
     def chemspider_hits(self):
         if not hasattr(self, '_chemspider_hits_data'):
             self._chemspider_hits_data = pd.read_sql_table('ChemSpiderResultItems', con=self.engine)
+            self._chemspider_hits_data['MolStructure'] = self._chemspider_hits_data['MolStructure'].apply(decode_mol_structure)
         return self._chemspider_hits_data
     
-    def get_chemspider_hits_for_feature(self, feature_ids):
+    def get_chemspider_search_results_for_feature(self, feature_ids):
         if len(feature_ids) == 1:
             where_clause = f"J.ConsolidatedUnknownCompoundItemsID = {feature_ids[0]}"
         elif len(feature_ids) > 1:
             where_clause = f"J.ConsolidatedUnknownCompoundItemsID IN {tuple(feature_ids)}"
         else:
             raise ValueError("feature_ids must contain at least one ID")
-        join = f"""SELECT J.ConsolidatedUnknownCompoundItemsID AS FeatureID, C.ChemSpiderID AS ChemSpiderID, J.DeltaMassInPPM AS DeltaMass, J.MzLogicScore AS Score, C.MolStructure AS Structure, C.Name AS Name, C.Formula AS Formula, C.MolecularWeight AS MolecularWeight, C.InChi AS InChi, C.InChiKey AS InChiKey
+        join = f"""SELECT J.ConsolidatedUnknownCompoundItemsID AS FeatureID, C.ChemSpiderID AS ChemSpiderID, C.Name AS Name, C.Formula AS Formula, C.MolecularWeight AS MolecularWeight, C.InChi AS InChi, C.InChiKey AS InChiKey, C.MolStructure AS Structure, J.DeltaMassInPPM AS DeltaMass, J.MzLogicScore AS Score, J.CompoundMatchStatus AS Status
                         FROM ConsolidatedUnknownCompoundItemsChemSpiderResultItems J JOIN ChemSpiderResultItems C 
                         ON J.ChemSpiderResultItemsChemSpiderID = C.ChemSpiderID
                         WHERE {where_clause}"""
-        return pd.read_sql_query(join, con=self.engine)
+        tab = pd.read_sql_query(join, con=self.engine)
+        tab['Structure'] = tab['Structure'].apply(decode_mol_structure)
+        return tab
+    
     
     """The following properties return tables that link the features to their best mzCloud hits"""
     @property
@@ -113,12 +145,14 @@ class PyDX(object):
     def mzcloud_search_results(self):
         if not hasattr(self, '_mzcloud_search_results_data'):
             self._mzcloud_search_results_data = pd.read_sql_table('MzCloudSearchResultItems', con=self.engine)
+            self._mzcloud_search_results_data['MolStructure'] = self._mzcloud_search_results_data['MolStructure'].apply(decode_mol_structure)
         return self._mzcloud_search_results_data
 
     @property
     def mzcloud_hits(self):
         if not hasattr(self, '_mzcloud_hits_data'):
             self._mzcloud_hits_data = pd.read_sql_table('MzCloudHitItems', con=self.engine)
+            self._mzcloud_hits_data['MolStructure'] = self._mzcloud_hits_data['MolStructure'].apply(decode_mol_structure)
         return self._mzcloud_hits_data
         
     def get_mzcloud_search_results_for_feature(self, feature_ids):
@@ -128,20 +162,78 @@ class PyDX(object):
             where_clause = f"J.ConsolidatedUnknownCompoundItemsID IN {tuple(feature_ids)}"
         else:
             raise ValueError("feature_ids must contain at least one ID")
-        join = f"""SELECT J.ConsolidatedUnknownCompoundItemsID AS FeatureID, M.MzCloudId AS MzCloudID, M.KeggId as KeggId, M.Name AS Name, M.Formula AS Formula, M.Mass AS MolecularWeight, M.MolStructure as Structure, J.DeltaMassInPPM AS DeltaMass, J.MzLibraryMatchFactor AS Score, J.Confidence AS Confidence, J.CompoundMatchStatus AS Status
+        join = f"""SELECT J.ConsolidatedUnknownCompoundItemsID AS FeatureID, M.MzCloudId AS MzCloudID, M.KeggId as KeggID, M.Name AS Name, M.Formula AS Formula, M.Mass AS MolecularWeight, M.MolStructure as Structure, J.DeltaMassInPPM AS DeltaMass, J.MzLibraryMatchFactor AS Score, J.Confidence AS Confidence, J.CompoundMatchStatus AS Status
                         FROM ConsolidatedUnknownCompoundItemsMzCloudSearchResultItems J JOIN MzCloudSearchResultItems M 
                         ON J.MzCloudSearchResultItemsID = M.ID
                         WHERE {where_clause}"""
-        return pd.read_sql_query(join, con=self.engine)
+        tab = pd.read_sql_query(join, con=self.engine)
+        tab['Structure'] = tab['Structure'].apply(decode_mol_structure)
+        return tab
     
     """The following properties return tables that link the features to their best hit ions and associated spectra"""
+    @property
+    def spectrum_ids(self):
+        if not hasattr(self, '_spectrum_ids'):
+            result = pd.read_sql_query("SELECT ID FROM MassSpectrumItems", con=self.engine)
+            self._spectrum_ids = result['ID'].tolist()
+        return self._spectrum_ids
+    
+    def filter_spectrum_ids(self, msn_level=None, activation_type=None, polarity=None):
+        where_clauses = []
+        if msn_level is not None:
+            where_clauses.append(f"MSOrder = {msn_level}")
+        if activation_type is not None:
+            where_clauses.append(f"ActivationType = '{activation_type}'")
+        if polarity is not None:
+            where_clauses.append(f"Polarity = '{polarity}'")
+        if len(where_clauses) > 0:
+            where_statement = " AND ".join(where_clauses)
+            result = pd.read_sql_query(f"SELECT ID FROM MassSpectrumItems WHERE {where_statement}", con=self.engine)
+        else:
+            result = pd.read_sql_query("SELECT ID FROM MassSpectrumItems", con=self.engine)
+        return result['ID'].tolist()
+        
+    @property
+    def spectra(self):
+        if not hasattr(self, '_spectra_data'):
+            self._spectra_data = pd.read_sql_table('MassSpectrumItems', con=self.engine)
+            self._spectra_data['Spectrum'] = self._spectra_data['Spectrum'].apply(decode_spectrum)
+        return self._spectra_data
+    
+    def get_spectra_by_id(self, spectrum_ids, asxml=False):
+        if type(spectrum_ids) == int: 
+            spectrum_ids = [spectrum_ids]
+        if len(spectrum_ids) == 1:
+            where_clause = f"MS.ID = {spectrum_ids[0]}"
+        elif len(spectrum_ids) > 1:
+            where_clause = f"MS.ID IN {tuple(spectrum_ids)}"
+        else:
+            raise ValueError("spectrum_ids must contain at least one ID")
+        
+        Q = f"""SELECT BHI.ConsolidatedUnknownCompoundItemsID AS FeatureID, MS.WorkflowID AS WorkflowID, MS.ID AS SpectrumID, MS.FileID AS FileID, MS.MSOrder AS MSn, MS.Polarity AS Polarity, MS.RetentionTime AS RetentionTime, MS.ResolutionAtMass200 AS Resolution, MS.ActivationType AS ActivationType, MS.ScanType AS ScanType, MS.Ionization AS Ionization, MS.MassAnalyzer AS MassAnalyzer, MS.Spectrum AS Spectrum
+                    FROM ConsolidatedUnknownCompoundItemsBestHitIonInstanceItems BHI 
+                        JOIN BestHitIonInstanceItemsMassSpectrumItems BHIMS 
+                            ON BHI.BestHitIonInstanceItemsWorkflowID = BHIMS.BestHitIonInstanceItemsWorkflowID 
+                                AND BHI.BestHitIonInstanceItemsID = BHIMS.BestHitIonInstanceItemsID
+                        JOIN MassSpectrumItems MS
+                            ON BHIMS.MassSpectrumItemsWorkflowID = MS.WorkflowID
+                                AND BHIMS.MassSpectrumItemsID = MS.ID
+        WHERE {where_clause}"""
+        
+        tab = pd.read_sql_query(Q, con=self.engine)
+        if asxml:
+            tab['Spectrum'] = tab['Spectrum'].apply(decode_xml_spectrum)
+        else:
+            tab['Spectrum'] = tab['Spectrum'].apply(decode_spectrum)
+        return tab
+    
     @property
     def best_hit_ion_annotations(self):
         if not hasattr(self, '_best_hit_ion_annotations_data'):
             self._best_hit_ion_annotations_data = pd.read_sql_table('ConsolidatedUnknownCompoundItemsBestHitIonInstanceItems', con=self.engine)
         return self._best_hit_ion_annotations_data
     
-    @property
+    @property 
     def best_hit_ions(self):
         if not hasattr(self, '_best_hit_ions_data'):
             self._best_hit_ions_data = pd.read_sql_table('BestHitIonInstanceItems', con=self.engine)
@@ -153,7 +245,7 @@ class PyDX(object):
             self._mass_spectra_ion_annotations_data = pd.read_sql_table('BestHitIonInstanceItemsMassSpectrumItems', con=self.engine)
         return self._mass_spectra_ion_annotations_data
     
-    def get_compound_spectra(self, feature_ids):
+    def get_compound_spectra(self, feature_ids, asxml=False):
         if len(feature_ids) == 1:
                 where_clause = f"BHI.ConsolidatedUnknownCompoundItemsID = {feature_ids[0]}"
         elif len(feature_ids) > 1:
@@ -169,9 +261,15 @@ class PyDX(object):
                             ON BHIMS.MassSpectrumItemsWorkflowID = MS.WorkflowID
                                 AND BHIMS.MassSpectrumItemsID = MS.ID
                 WHERE {where_clause}"""
-        return pd.read_sql_query(Q, con=idxa.engine)
-                
+        tab = pd.read_sql_query(Q, con=self.engine)
+        if asxml:
+            tab['Spectrum'] = tab['Spectrum'].apply(decode_xml_spectrum)
+        else:
+            tab['Spectrum'] = tab['Spectrum'].apply(decode_spectrum)
+        return tab
     
+    
+    """Utility function to print the schema of a table for debugging purposes"""
     def show_table_schema(self, table_name):
         print(f"Table: {table_name}")
         inspector = inspect(self.engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -6,8 +6,8 @@ def test_fetch_chemspider_annotations_for_feature_set():
     
     feature_IDs = list(range(1,20))
     
-    result = idxa.get_chemspider_hits_for_feature(feature_IDs)    
-    assert result.columns.tolist() == ['FeatureID', 'ChemSpiderID', 'DeltaMass', 'Score', 'Structure', 'Name', 'Formula', 'MolecularWeight', 'InChi', 'InChiKey']
+    result = idxa.get_chemspider_search_results_for_feature(feature_IDs)    
+    assert result.columns.tolist() == ['FeatureID', 'ChemSpiderID', 'Name', 'Formula', 'MolecularWeight', 'InChi', 'InChiKey', 'Structure', 'DeltaMass', 'Score', 'Status']
     
 def test_get_mzcloud_search_results_for_feature_set():
     from pydx import PyDX
@@ -16,4 +16,43 @@ def test_get_mzcloud_search_results_for_feature_set():
     feature_IDs = list(range(1,20))
     
     result = idxa.get_mzcloud_search_results_for_feature(feature_IDs)
-    assert result.columns.tolist() == ['FeatureID', 'MzCloudID', 'KeggId', 'Name', 'Formula', 'MolecularWeight', 'Structure', 'DeltaMass', 'Score', 'Confidence', 'Status']
+    assert result.columns.tolist() == ['FeatureID', 'MzCloudID', 'KeggID', 'Name', 'Formula', 'MolecularWeight', 'Structure', 'DeltaMass', 'Score', 'Confidence', 'Status']
+    
+def test_get_features_decodes_all_fields():
+    from pydx import PyDX
+    idxa = PyDX(TEST_DATABASE_FILE) # Replace this with a smaller test database in the future
+    
+    feature = idxa.get_feature_by_name('Benzylfentanyl').iloc[0]
+    assert feature['Area'].shape == (107,)
+    assert feature['AreaFlags'].shape == (107,)
+    assert feature['PeakRating'].shape == (107,)
+    assert feature['PeakRatingFlags'].shape == (107,)
+    assert feature['GapFillStatus'].shape == (107,)
+    assert feature['GapStatus'].shape == (107,)
+    assert isinstance(feature['MolStructure'], str)
+    
+def test_get_spectra_for_feature_decodes_spectra():
+    import pandas as pd
+    from pydx import PyDX
+    idxa = PyDX(TEST_DATABASE_FILE) # Replace this with a smaller test database in the future
+    
+    fid = idxa.get_feature_by_name('Benzylfentanyl').iloc[0].ID
+    spectra = idxa.get_compound_spectra(feature_ids=[fid]).Spectrum
+    for i in range(len(spectra)):
+        assert isinstance(spectra.iloc[i], pd.DataFrame)
+        assert spectra.iloc[i].columns.tolist() == ["mz", "intensity", "Z", "resolution", "signalNoiseRatio"]
+    
+def test_get_retention_time_corrections_returns_decoded_curves():
+    import numpy as np
+    from pydx import PyDX
+    idxa = PyDX(TEST_DATABASE_FILE) # Replace this with a smaller test database in the future
+    
+    corrections = idxa.corrected_retention_times
+    for i in range(len(corrections)):
+        oRT = corrections.iloc[i]['OriginalRT']
+        cRT = corrections.iloc[i]['CorrectedRT']
+        
+        assert isinstance(oRT, np.ndarray)
+        assert isinstance(cRT, np.ndarray)
+        
+        assert oRT.shape == cRT.shape
