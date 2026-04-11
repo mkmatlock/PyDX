e306dfdd2373ba98651e817852016da1cfe53d7e
Matthew Matlock <matt.matlock@gmail.com>
2026-04-10 21:10:05 -0500

    Fix embedding export

--- pydx/dreams.py
+++ pydx/dreams.py
@@ -1,3 +1,5 @@
+import pdb
+
 from dreams.utils.data import MSData
 from dreams.api import dreams_embeddings
 import numpy as np
@@ -16,8 +18,8 @@ def embed(spectra_file, output_file):
     
     print("Saving embeddings to HDF5...")
     with h5py.File(output_file, 'w') as hf:
-        hf.create_dataset('spectrum_id', data=msd['spectrum_id'].values)
-        hf.create_dataset('embedding', data=np.stack(embeddings['Embedding'].values))
+        hf.create_dataset('spectrum_id', data=msd['spectrum_id'][:])
+        hf.create_dataset('embedding', data=embeddings)
 
 if __name__ == '__main__':
     embed()
